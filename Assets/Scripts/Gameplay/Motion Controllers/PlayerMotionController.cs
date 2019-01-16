5588faf86f07bd18daa09760b1373f20b8b9e56b
ScarletPhoenix5053 <45509046+ScarletPhoenix5053@users.noreply.github.com>
2019-01-16 23:14:41 +1300

    variable height jump

---
+++ Assets/Scripts/Gameplay/Motion Controllers/PlayerMotionController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMotionController : CharacterMotionController
+{
+    public float FallMultiplier = 1.5f;
+    public float LowJumpMultiplier = 1.5f;
+
+    protected override void ApplyGravity()
+    {
+        if (IsGrounded)
+        {
+            if (contMotionVector.y < 0f) contMotionVector.y = -0.5f;
+        }
+        // If not falling faster than max falling speed
+        else if (contMotionVector.y > -GravityMax)
+        {
+            // ...and is actually falling, not rising
+            if (contMotionVector.y < 0)
+                contMotionVector.y -= Gravity * FallMultiplier;
+            // ...otherwise must be rising
+            else if
+                (contMotionVector.y > 0 && !Input.GetKey(KeyCode.W))
+                contMotionVector.y -= Gravity * LowJumpMultiplier;
+            else
+                contMotionVector.y -= Gravity;
+        }
+    }
+}
