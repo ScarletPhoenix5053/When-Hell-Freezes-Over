9e3aa1644ffd6f39635f307d15fd81e25a29312c
ScarletPhoenix5053 <45509046+ScarletPhoenix5053@users.noreply.github.com>
2019-01-17 01:07:47 +1300

    editor scripting
    allowed weapon items to render their icon in inspector.

---
+++ Assets/Editor/Items/RangedWeaponItemEditor.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(RangedWeaponItem))]
+public class RangedWeaponItemEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        var weaponItem = target as RangedWeaponItem;
+        GUILayout.BeginVertical(GUILayout.Width(48));
+        GUILayout.Label(TextureFromSprite(weaponItem.Icon), GUILayout.Width(48), GUILayout.Height(48));
+        GUILayout.EndVertical();
+        base.OnInspectorGUI();
+    }
+    public static Texture2D TextureFromSprite(Sprite sprite)
+    {
+        if (sprite.rect.width != sprite.texture.width)
+        {
+            Texture2D newText = new Texture2D((int)sprite.rect.width, (int)sprite.rect.height);
+            Color[] newColors = sprite.texture.GetPixels((int)sprite.textureRect.x,
+                                                         (int)sprite.textureRect.y,
+                                                         (int)sprite.textureRect.width,
+                                                         (int)sprite.textureRect.height);
+            newText.SetPixels(newColors);
+            newText.Apply();
+            return newText;
+        }
+        else
+            return sprite.texture;
+    }
+}
--- Assets/Scripts/Items/MeleeWeaponItem.cs
+++ Assets/Scripts/Items/MeleeWeaponItem.cs
@@ -3,6 +3,8 @@
 [CreateAssetMenu(fileName = "New Weapon Data", menuName = "Item/Weapon")]
 public class MeleeWeaponItem : GenericItem
 {
+    [HideInInspector]
+    public int NormalAttackChainLength;
     public AttackData[] NormalAtkData;
     public AttackData SpecialAtkData;
     public WeaponType Type;
@@ -12,5 +14,19 @@ public class MeleeWeaponItem : GenericItem
     public enum WeaponType
     {
         LongSword, GreatSword, Mace, Warhammer
+    }
+
+    public void Refresh()
+    {
+        var newNormalAtkData = new AttackData[NormalAttackChainLength];
+
+        if (NormalAtkData != null)
+        {
+            for (int i = 0; i < Mathf.Min(newNormalAtkData.Length, newNormalAtkData.Length); i++)
+            {
+                newNormalAtkData[i] = NormalAtkData[i];
+            }
+        }
+        NormalAtkData = newNormalAtkData;
     }
 }
