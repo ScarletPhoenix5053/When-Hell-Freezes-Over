193382e8b86a12e853f3b0555698aacb2d6863d4
ScarletPhoenix5053 <45509046+ScarletPhoenix5053@users.noreply.github.com>
2019-01-17 13:23:24 +1300

    bounce pad complete
    bounce pad prefab now works

---
+++ Assets/Scripts/Gameplay/Environment/BouncePad.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BouncePad : MonoBehaviour
+{
+    public float DelayBetweenActivations = 1f;
+    public float Velocity = 20f;
+
+    protected Collider2D trigger;
+    protected CharacterMotionController otherMC;
+
+    protected float timeSinceLastActivation;
+
+    private void FixedUpdate()
+    {
+        IncrimentActivationTimer();
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // If player
+        if (other.tag == "Player" /*&& timeSinceLastActivation >= DelayBetweenActivations*/)
+        {
+            Debug.Log("hi");
+            // Apply impluse
+            otherMC = other.GetComponent<CharacterMotionController>();
+            otherMC.DoImpulse(Vector2.up * Velocity);
+
+            // timer
+            timeSinceLastActivation = 0;
+        }
+    }
+
+    protected void IncrimentActivationTimer()
+    {
+        if (timeSinceLastActivation < DelayBetweenActivations) timeSinceLastActivation += Time.fixedDeltaTime;
+    }
+}
