5b4319de5d8bd54b0149dd5fa9da6497232e9d4a
ScarletPhoenix5053 <45509046+ScarletPhoenix5053@users.noreply.github.com>
2019-01-29 14:58:30 +1300

    completed changer script using cinemachine
    blend transitions could use some refinement but that's of low priority right now

---
+++ Assets/Scripts/Camera/CameraChangeTester.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraChangeTester : MonoBehaviour
+{
+    public CameraChanger CameraChanger;
+    public CinemachineVirtualCameraBase[] VCams;
+
+    bool alternate;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            if (alternate) CameraChanger.ChangeTo(VCams[0]);
+            else CameraChanger.ChangeTo(VCams[1]);
+
+            alternate = !alternate;
+        }
+    }
+}
