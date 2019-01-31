f8ace53a8874c79491d1dd2c43b88721cc96fd84
ScarletPhoenix5053 <45509046+ScarletPhoenix5053@users.noreply.github.com>
2019-02-01 12:42:49 +1300

    begin spine event implimentation

---
+++ Assets/Scripts/Gameplay/Combat System/Hit Detection/Hitbox_Player.cs
@@ -0,0 +1,19 @@
+﻿using Sierra.Combat2D;
+using Spine.Unity;
+
+public class Hitbox_Player : Hitbox
+{
+    private SkeletonAnimation sk_an;
+    private void Awake()
+    {
+        sk_an.state.Event += State_Event;
+    }
+
+    private void State_Event(Spine.TrackEntry trackEntry, Spine.Event e)
+    {
+        if (e.Data.Name == "Axe")
+        {
+
+        }
+    }
+}
