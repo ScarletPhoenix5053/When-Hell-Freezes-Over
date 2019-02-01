900c25982456e98a45f503c93094b5f18f2c1855
ScarletPhoenix5053 <45509046+ScarletPhoenix5053@users.noreply.github.com>
2019-02-01 14:52:45 +1300

    all attacks have custom hitboxes

--- Assets/Scripts/Gameplay/Combat System/Hit Detection/Hitbox_Player.cs
+++ Assets/Scripts/Gameplay/Combat System/Hit Detection/Hitbox_Player.cs
@@ -19,12 +19,28 @@ public class Hitbox_Player : Hitbox
     
     private void ChangeHitboxShapeOnEvent(Spine.TrackEntry trackEntry, Spine.Event e)
     {
-        const int AnimCount = 2;
+        const int AnimCount = 10;
         if (SizeOnAnims.Length != AnimCount) throw new IndexOutOfRangeException();
 
         var eventName = e.Data.Name;
+
+        // Greataxe
         if (eventName == "Axe1") SetPosAndSize(0);
         else if (eventName == "Axe2") SetPosAndSize(1);
+        
+        // Mace
+        else if (eventName == "Mace1") SetPosAndSize(2);
+        else if (eventName == "Mace2") SetPosAndSize(3);
+        else if (eventName == "Mace3") SetPosAndSize(4);
+
+        // Light Sword
+        else if (eventName == "LSword1") SetPosAndSize(5);
+        else if (eventName == "LSword2") SetPosAndSize(6);
+        else if (eventName == "LSword3") SetPosAndSize(7);
+        else if (eventName == "LSword4") SetPosAndSize(8);
+        else if (eventName == "LSword5") SetPosAndSize(9);
+
+        // GreatSword
     }
 
     public void SetPosAndSize(int index)
