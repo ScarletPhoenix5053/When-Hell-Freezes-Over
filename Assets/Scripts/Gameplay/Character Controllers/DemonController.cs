14417a3a937a98549dcb1f9f32c4f929193b8728
ScarletPhoenix5053 <45509046+ScarletPhoenix5053@users.noreply.github.com>
2019-02-07 14:49:57 +1300

    demon start

---
+++ Assets/Scripts/Gameplay/Character Controllers/DemonController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AttackManager))]
+public class DemonController : EnemyController
+{
+
+    public Behaviour CurrentBehaviour;
+    public enum Behaviour
+    {
+        Idle, Throwing
+    }
+
+    public void SetBehaviour(Behaviour newBehaviour)
+    {
+        if (newBehaviour != CurrentBehaviour)
+            CurrentBehaviour = newBehaviour;
+    }
+
+    protected override void Act()
+    {
+        throw new System.NotImplementedException();
+    }
+    protected override void DecideAction()
+    {
+        throw new System.NotImplementedException();
+    }
+}
